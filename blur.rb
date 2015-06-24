0ef8e152d74c9cdefec10eaf6070574e70b5597c
Colin Rubbert <colinrubbert@gmail.com>
2015-06-24 01:15:59 +0000

    Challenges update

---
+++ blur.rb
@@ -0,0 +1,36 @@
+# Mentor session explanations 
+
+class Xyz
+  def thing dist
+    (0..dist).each do |y|
+      (0..(dist-y)).each do |x|
+        puts "d: #{y}, #{x}"
+      end
+    end
+  end
+end
+
+x = Xyz.new
+x.thing 3
+
+a = 1
+b = a
+a = 2
+puts b
+1
+
+a = Thing.new
+
+a -> [OBJEC
+
+b = a
+
+c = THing.new
+b = c
+
+
+a.MethodThatChangesA
+puts b
+
+number is primitive
+b=M.load(M.dump(a))
