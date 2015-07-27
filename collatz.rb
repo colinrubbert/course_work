60e349982417479ea58a0fd1a025a89a1ab8d7ed
Colin Rubbert <colinrubbert@gmail.com>
2015-07-27 17:37:50 -0500

    Add collatz coding challenge

---
+++ collatz.rb
@@ -0,0 +1,34 @@
+def collatz(n)
+  longest_sequence = []
+  longest_sequence << n
+
+  # Iterative Collatz Sequence
+  until longest_sequence.last == 1
+    if longest_sequence.last % 2 == 0
+      longest_sequence << (longest_sequence.last)/2
+    else longest_sequence.last
+      longest_sequence << 3*(longest_sequence.last)+1
+    end
+  end
+
+  # Prints out the Sequence Numbers
+  # longest_sequence.each do |ls|
+  #   if ls == 1
+  #     puts ls
+  #   else
+  #     puts "#{ls} --> "
+  #   end
+  # end
+
+  puts "Sequence length is #{longest_sequence.count}"
+end
+
+collatz(1)
+collatz(2)
+collatz(3)
+collatz(4)
+collatz(5)
+collatz(6)
+collatz(7)
+collatz(256)
+collatz(257)
