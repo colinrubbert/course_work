2b2bba94d14b14f8ce27f54da0322cb4c4d79819
Colin Rubbert <colinrubbert@gmail.com>
2015-08-01 16:59:15 -0500

    Project Euler Problem #2

--- fibonacci_even_numbers.rb
+++ fibonacci_even_numbers.rb
@@ -1,9 +1,9 @@
-def fibonnaci(n)
+def fibonacci(n)
   x = 0
   y = 1
-  fibonnaci_numbers = []
-  fibonnaci_numbers_even = []
-  sum_of_fibonnaci_even_numbers = []
+  fibonacci_numbers = []
+  fibonacci_numbers_even = []
+  sum_of_fibonacci_even_numbers = []
 
   if n == 0
     return n
@@ -11,23 +11,23 @@ def fibonnaci(n)
     (1..n).each do
       z = x
       x = y
-      fibonnaci_numbers << x
+      fibonacci_numbers << x
       y = z + y
     end
   end
 
-  fibonnaci_numbers.each do |f|
+  fibonacci_numbers.each do |f|
     if f % 2 == 0
-      fibonnaci_numbers_even << f
+      fibonacci_numbers_even << f
     end
   end
 
-  sum_of_fibonnaci_even_numbers = fibonnaci_numbers_even.inject do |sum, x|
+  sum_of_fibonacci_even_numbers = fibonacci_numbers_even.inject do |sum, x|
      break sum if sum > 4000000
      sum + x
    end
 
-  puts sum_of_fibonnaci_even_numbers
+  puts sum_of_fibonacci_even_numbers
 
 end
-fibonnaci(300)
+fibonacci(300)
