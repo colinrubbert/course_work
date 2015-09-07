0dd322afba56412a01e0075be9af99f446f760ed
Colin Rubbert <colinrubbert@gmail.com>
2015-09-07 17:39:35 -0500

    Add Ruby Koans

---
+++ largest_prime_factor.rb
@@ -0,0 +1,8 @@
+def recursive_primes(n)
+  primes_factors = []
+
+  # if (n > 2 && n % 2 == 0) || (n > 3 && n % 3 == 0) || (n > 5 && n % 5 == 0) || (n > 7 && n % 7 == 0)
+  if n ==
+    return n
+  end
+end
